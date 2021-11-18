b20ffa047205284dcfab737bdd7173b22caee831
Michael Carr <xyeres@me.com>
2021-11-18 18:31:04 -0500

    Working solution

---
+++ challenges/matchingParentheses.js
@@ -0,0 +1,64 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+function Stack(array) {
+  this.array = [];
+
+  if (array) {
+    this.array = array;
+  }
+}
+
+Stack.prototype.pop = function () {
+  return this.array.pop()
+}
+
+Stack.prototype.push = function (value) {
+  this.array.push(value)
+}
+
+Stack.prototype.isEmpty = function () {
+  return this.array.length === 0;
+}
+
+Stack.prototype.peek = function() {
+  return this.array[this.array.length - 1];
+}
+
+var isValid = function (s) {
+  const types = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  }
+
+  let lastElement = null;
+
+  // create a buffer stack to keep track of the parentheses
+  let bufferStack = new Stack();
+
+  // Loop through the length of s
+  for (let i = 0, stringLen = s.length; i < stringLen; i++) {
+    let currentElement = s.charAt(i);
+
+    // If opening parentheses:    
+    if (currentElement in types) {
+      bufferStack.push(currentElement);
+      lastElement = currentElement;
+
+    } else { // it must be a closing element
+      if (types[bufferStack.peek()] === currentElement) {
+        bufferStack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+  return bufferStack.isEmpty();
+};
+
+console.log(isValid("(){}}{")) // false
+console.log(isValid("(({}))")) // true
+console.log(isValid("([)]{}")) // false
