aca9f0146c34a3b0b18a42313fb65452a541bf55
Michael Carr <xyeres@me.com>
2021-11-24 12:18:02 -0500

    Add basic closure examples

--- concepts/closure.js
+++ concepts/closure.js
@@ -10,7 +10,7 @@
 
 function thingMaker(animalType) {
   let count = 0;
-  
+
   return function inner() {
     count++;
     console.log(`Hello, i am a ${animalType}, I am the ${count} version of me.`)
@@ -20,11 +20,19 @@ function thingMaker(animalType) {
 const bigRedDog = thingMaker('big red dog')
 const smallWhiteDog = thingMaker('small teeny tiny doggie')
 
-bigRedDog()
+smallWhiteDog()
+smallWhiteDog()
+smallWhiteDog()
+smallWhiteDog()
 smallWhiteDog()
 bigRedDog()
-smallWhiteDog()
-bigRedDog()
-bigRedDog()
-bigRedDog()
-bigRedDog()
+
+
+// Make add(a)(b) work
+function add(a) {
+  return function (b) {
+    return a + b
+  }
+}
+
+console.log(add(2)(5))
