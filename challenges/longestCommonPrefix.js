057ac20b3349bc80c9f981e3ef6569267f97c2a8
Michael Carr <xyeres@me.com>
2021-12-01 12:32:57 -0500

    Add comments

--- challenges/longestCommonPrefix.js
+++ challenges/longestCommonPrefix.js
@@ -19,24 +19,26 @@ Explanation: There is no common prefix among the input strings.
  * @return {string}
  */
 
-// Solution using horizontal scanning
-var longestCommonPrefix = function (strs) {
-  if (strs.length === 0) return '';
-  let prefix = strs[0]; // use 1st word as prefix base case
-
-  for (let i = 1; i < strs.length; i++) { // loop through remaining words
-    while(strs[i].indexOf(prefix) != 0) { // while current word is not contained in prefix
-      prefix = prefix.substring(0, prefix.length - 1); // trim 1 character off the prefix and check again
-      if (prefix.length < 2) return ''; // if the prefix is less than 2 chars, a common prefix doesn't exist so return ''
-    }
-  }
-  return prefix; // if prefix remains then it is the LCP, return it!
-};
-
-let strs = ["dvgg",
-  "dvgracecar",
-  "dzgcar",
+let strs = ["dagg",
+  "dagracecar",
+  "dagcar",
   "dad"]
 
+var longestCommonPrefix = function (strs) {
+  // Use 1st word as prefix base
+  let prefix = strs[0];
 
+  // Loop through remaining words
+  for (let i = 1; i < strs.length; i++) {
+    // While the current prefix does not exist in the current word,
+    while(strs[i].indexOf(prefix) != 0) {
+      // trim the prefix by 1 character until there is a common prefix or the len of prefix is 0,
+      prefix = prefix.substring(0, prefix.length - 1);
+        // return '' if len of prefix is less than 2
+      if (prefix.length < 2) return '';
+    }
+  }
+  // else return remaining prefix if one exists after looping through all words
+  return prefix;
+}
 console.log(longestCommonPrefix(strs))
