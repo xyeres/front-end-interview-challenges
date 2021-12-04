a6e711fb55999403985fb3b213e713be1d68216f
Michael Carr <xyeres@me.com>
2021-12-04 12:04:20 -0500

    Add another version of the O(n) solution

--- challenges/arrayOfProducts.js
+++ challenges/arrayOfProducts.js
@@ -41,21 +41,26 @@ console.log(findProductOn2([1, 2, 3, 4]))
 // O(n) Linear Time solution
 function findProductOn(arr) {
 
-  var temp = 1,
-    product = [];
-  for (var i = 0; i < arr.length; i++) {
-    product[i] = temp;
-    temp = temp * arr[i];
-  }
-  
-  temp = 1;
-  for (var j = arr.length -1; j > -1; --j) {
-    product[j] *= temp;
-    temp *= arr[j];
-  }
-  
-  return product;
+  // loop through arr from start to end
+  // store in new product[i] array the product of all elements to the 
+  // left of arr[i]
+  // use tmp to store 
+  let product = [];
+  let tmp = 1;
 
+  for (let i = 0; i < arr.length; i++) {
+    product[i] = tmp;
+    tmp = tmp * arr[i];
+  }
+
+  // loop through arr from end to start,
+  // multiply all numbesr to right of arr[i]
+  tmp = 1;
+  for (let i = arr.length - 1; i > -1; i--) {
+    product[i] = product[i] * tmp;
+    tmp = tmp * arr[i];
+  }
+  return product;
 }
 
-console.log(findProductOn([1, 3, 4, 5]))
+console.log(findProductOn([1, 3, 4, 5])) // tmp = 1, 1, 3, 12 // tmp = 1, 5, 20, 60// product = [60, 20, 15, 12]
