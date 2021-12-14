3c095a3407bbae21a30b495424caf17d23c173dc
Michael Carr <xyeres@me.com>
2021-12-14 11:23:59 -0500

    Reset challenge

--- concepts/async/rewrite.js
+++ concepts/async/rewrite.js
@@ -1,16 +1,15 @@
 // Rewrite this example code from the chapter Promises chaining using async/await instead of .then/catch:
 // https://javascript.info/async-await#rewrite-using-async-await
 
-
-async function loadJson(url) {
-  let response = await fetch(url)
-
-  if (response.status == 200) {
-    let json = await response.json()
-    return json;
-  }
-  
-  throw new Error(response.status);
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    });
 }
 
 loadJson('no-such-user.json')
--- concepts/async/rewrite2.js
+++ concepts/async/rewrite2.js
@@ -4,7 +4,6 @@
 
 // https://javascript.info/async-await#rewrite-using-async-await
 
-
 class HttpError extends Error {
   constructor(response) {
     super(`${response.status} for ${response.url}`);
@@ -13,39 +12,34 @@ class HttpError extends Error {
   }
 }
 
-async function loadJson(url) {
-  let response = await fetch(url)
-  if (response.status == 200) {
-    return response.json();
-  } else {
-    throw new HttpError(response);
-  }
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new HttpError(response);
+      }
+    });
 }
 
 // Ask for a user name until github returns a valid user
-async function demoGithubUser() {
+function demoGithubUser() {
   let name = prompt("Enter a name?", "iliakan");
-  let user;
 
-  while(true) {
-    try {
-      user = await loadJson(`https://api.github.com/users/${name}`)
-      break;
-    }
-    catch (err) {
+  return loadJson(`https://api.github.com/users/${name}`)
+    .then(user => {
+      alert(`Full name: ${user.name}.`);
+      return user;
+    })
+    .catch(err => {
       if (err instanceof HttpError && err.response.status == 404) {
         alert("No such user, please reenter.");
         return demoGithubUser();
       } else {
         throw err;
       }
-    }
-  }
-
-
-  alert(`Full name: ${user.name}.`);
-  return user;
-
+    });
 }
 
 demoGithubUser();
