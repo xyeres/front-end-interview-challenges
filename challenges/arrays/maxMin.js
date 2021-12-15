83c5f3544671bb98bc5116246a31d4420903079a
Michael Carr <xyeres@me.com>
2021-12-15 12:21:26 -0500

    Improved solution

--- challenges/arrays/maxMin.js
+++ challenges/arrays/maxMin.js
@@ -45,16 +45,23 @@ function maxMin(arr) {
 }
 
 
-// Educative Solution
+// Improved Solution
 function maxMin(arr) {
-  var result = []
-  for (var i = 0; i < (Math.floor(arr.length / 2)); i++) {
-    result.push(arr[arr.length - (i + 1)])
-    result.push(arr[i])
+
+  let halfPoint = Math.floor(arr.length / 2)
+  let result = [];
+
+  for (let i = 0; i < halfPoint; i++) {
+    let firstElement = arr[i];
+    let lastElement = arr[(arr.length - (1 + i))];
+    result.push(lastElement)
+    result.push(firstElement)
   }
 
-  if (arr.length % 2)
-    result.push(arr[Math.floor(arr.length / 2)])
-  return result
+  if (arr.length % 2) { // odd arr length 
+    result.push(arr[halfPoint])
+  }
+
+  return result;
 }
 console.log(maxMin([1, 2, 3, 4, 5, 6]))
