f8f7a42d02232734a95ed665b115aba6b55d04d5
Michael Carr <xyeres@me.com>
2022-01-04 13:23:00 -0800

    Add solution

---
+++ challenges/curry.js
@@ -0,0 +1,41 @@
+/**
+ * Problem statement#
+    Write a wrapper function currying which accepts a function, say func, and returns the curried version of func.
+
+    Input#
+    Function to be transformed to the currying function
+
+    Output#
+    Curried Function
+
+    Sample input#
+    function multiply(a, b, c) {
+        return a*b*c;
+    }
+    Sample output#
+    let curried = currying(multiply);
+    curried(2)(3)(4) //24
+    curried(2,3)(4) //24
+ */
+
+
+function currying(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args)
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2))
+      }
+    }
+  }
+}
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+let curried = currying(multiply);
+console.log(curried(2)(3)(4)) //24
+let curriedFixedVales = curried(2, 3)
+console.log(curriedFixedVales(2)) //12
