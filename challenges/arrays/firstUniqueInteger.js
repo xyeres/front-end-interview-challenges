e5039451a7ad80a6be161c2ccab2a338a00e0a12
Michael Carr <xyeres@me.com>
2022-06-08 10:11:18 -0700

    Clean up

--- challenges/arrays/firstUniqueInteger.js
+++ challenges/arrays/firstUniqueInteger.js
@@ -1,44 +1,25 @@
-function findFirstUniqueMap(arr) {
-  // let map = new Map(); // use ES6 map because it preserves order
-  // let stack = [];
-
-  // arr.forEach((num) => {
-  //   if (map.has(num)) {
-  //     map.set(num, map.get(num) + 1)
-  //   } else {
-  //     map.set(num, 1)
-  //   }
-  // })
-
-  // for (const [key, value] of map) {
-  //   if (value === 1) return key; // remember maps are ordered, so the first val === 1 is our first unique integer
-  // }
-
-  //
-}
-
 function findFirstUnique(arr) {
-  // loop through array as i
-  // loop through remaining of array as j
-    // if i != j && arr[i] == arr[j], break out
-  // if j === arr.length // if we get to the end and still havent found a match, then it is unique
-    // return arr[i]
-  // otherwise return NULL
-  
-  let j;
-  for (let i = 0; i < arr.length; i++) {
-    j = 0
-    while(j < arr.length) {
-      if (i != j && arr[i] === arr[j]) {
-        break
-      }
-      j++
+  // create a map,
+  const arrMap = new Map();
+
+  arr.forEach((v) => {
+    if (arrMap.has(v)) {
+      arrMap.set(v, 0)
+    } else {
+      arrMap.set(v, 1)
     }
-    if (j === arr.length) {
-      return arr[i]
+  })
+  // add each item in the array as a key, val as 1 to start
+  // if item is encounted again, make val = 0
+
+  for (let [key, val] of arrMap) {
+    if (val === 1) {
+      return key
     }
   }
-  return null
+
+  return -1
+  // loop through map, if value == 0, return that key
 }
 
-console.log(findFirstUnique([2, 9, 1, 2, 6, 6, 9])) // expected output: 9
+console.log(findFirstUnique([1, 1, 1, 2,2, 6, 6, 9])) // expected output: 1
