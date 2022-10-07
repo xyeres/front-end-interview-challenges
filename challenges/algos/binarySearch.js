20361af6502891ac5dd0bad62365629f34a2e633
Michael Carr <xyeres@me.com>
2022-10-07 15:00:58 -0700

    clean: make easier to read

--- challenges/algos/binarySearch.js
+++ challenges/algos/binarySearch.js
@@ -1,10 +1,10 @@
 function binarySearch(nums, target) {
+    'use strict';
     if (!nums || nums.length === 0) {
         return -1
     }
 
-    let left = 0;
-    let right = nums.length;
+    var left = 0, right = nums.length;
 
     while (left + 1 < right) {
         let mid = Math.floor((right + left) / 2)
@@ -17,16 +17,17 @@ function binarySearch(nums, target) {
         }
     }
 
+    // be sure to check that nums[left] is not target,
     if (nums[left] == target) {
         return left;
     }
-    // be sure to check that nums[left] is not target,
 
-    return -1
     // otherwise return -1
+    return -1
 }
+
 
 const nums = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
 
-console.log(`Index of 37 is ---> ${binarySearch(nums, 37)}`);
-console.log(`Index of 1 is ---> ${binarySearch(nums, 1)}`);
+console.log(`Index of 37 is ---> ${binarySearch(nums, 37)}`); // 11
+console.log(`Index of 1 is ---> ${binarySearch(nums, 5)}`); // 2
