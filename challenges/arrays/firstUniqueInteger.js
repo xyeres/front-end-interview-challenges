19831ea7c6c7a9c9e05c60917a1951f563506e29
Michael Carr <xyeres@me.com>
2022-10-31 14:57:41 -0700

    add: problem statement

--- challenges/arrays/firstUniqueInteger.js
+++ challenges/arrays/firstUniqueInteger.js
@@ -1,3 +1,19 @@
+// Problem Statement#
+// Implement a function, findFirstUnique(arr), which takes an array as input and returns the first unique integer in the array.
+
+// Note: The function should return null if no unique number is found.
+
+// Input#
+// An array of integers
+
+// Output#
+// The first unique element in the array
+
+// Sample Input#
+// [9,2,3,2,6,6]
+// Sample Output#
+// 9
+
 function findFirstUnique(arr) {
   // Using map
   // map preserves order of elements added, so we can loop back through it at the end and return the first placed original item
