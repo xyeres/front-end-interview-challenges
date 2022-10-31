31d67b5485eed231d607b5dd39794d42130a1021
Michael Carr <xyeres@me.com>
2022-10-31 14:57:46 -0700

    clean: solution

--- challenges/arrays/fizzbuzz.js
+++ challenges/arrays/fizzbuzz.js
@@ -1,21 +1,18 @@
-
 // For Loop Solution
-for (var i = 0; i < 101; i++) {
-  if (i > 0) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log('fizbuzz')
-    } else if (i % 3 == 0) {
-      console.log('fizz')
-    } else if (i % 5 == 0) {
-      console.log('buzz')
-    } 
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fizz buzz", i);
+  } else if (i % 3 == 0) {
+    console.log("fiz", `(${i})`);
+  } else if (i % 5 == 0) {
+    console.log("buzz", i);
   }
 }
 
 // Ternary Expression Solution
 
-for (let i =1; i <= 100; i++) {
-  i % 3 == 0 && i % 5==0 ? console.log('fizbuzz', i) : null
-  i % 3 == 0 ? console.log('fizz', i) : null 
-  i % 5 == 0 ? console.log('buzz', i) : null
-}
+// for (let i = 1; i <= 100; i++) {
+//   i % 3 == 0 && i % 5 == 0 ? console.log("fizbuzz", i) : null;
+//   i % 3 == 0 ? console.log("fizz", i) : null;
+//   i % 5 == 0 ? console.log("buzz", i) : null;
+// }
