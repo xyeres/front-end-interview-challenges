6f1de238741810dfac4b7e4029d93d2f57eafc40
Michael Carr <xyeres@me.com>
2022-11-04 13:36:51 -0700

    add: in memory db solution

---
+++ utlities/inMemoryDb.ts
@@ -0,0 +1,37 @@
+interface Database {
+  get(id: string): string;
+  set(id: string, value: string): void;
+}
+
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storedState: string): void;
+}
+
+class InMemoryDB implements Database {
+  protected db: Record<string, string> = {};
+
+  get(id: string): string {
+    return this.db[id];
+  }
+
+  set(id: string, value: string): void {
+    this.db[id] = value;
+  }
+}
+
+class PersistantMemDB extends InMemoryDB implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db);
+  }
+
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState);
+  }
+}
+
+let db = new PersistantMemDB();
+
+db.set("foo", "bar")
+db.restoreFromString(`{"test": "value"}`)
+console.log(db.get("test"))
